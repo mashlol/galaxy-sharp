f388503ef0f276a0f06082968d042ff991cc830e
Kevin Bedi <kevin.m.bedi@gmail.com>
2013-03-22 23:45:10 -0400

    Add map init support

--- triggers.js
+++ triggers.js
@@ -4,5 +4,11 @@ module.exports = {
 		params: ["Player", "string"],
 		prepend: "int $0 = EventPlayer(); string $1 = EventChatMessage(false);",
 		triggerMethod: "TriggerAddEventChatMessage"
+	},
+	onMapInit: {
+		argNum: 0,
+		params: [],
+		prepend: "",
+		triggerMethod: "TriggerAddEventMapInit"
 	}
 }
